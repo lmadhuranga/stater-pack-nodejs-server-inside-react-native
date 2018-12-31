7449d0f37bdfa6fd95189b656999a85bef47c371
lmadhuranga <lilan.maduranga@gmail.com>
2018-12-31 10:15:17 +0530

    Init commit

---
+++ nodejs-assets/nodejs-project/main.js
@@ -0,0 +1,11 @@
+var rn_bridge = require('rn-bridge');
+
+// Echo every message received from react-native.
+rn_bridge.channel.on('message', (msg) => {
+  rn_bridge.channel.send(msg);
+} );
+
+// Inform react-native node is initialized.
+rn_bridge.channel.send("Wow Node was initialized.", () => {
+  console.log('send msg')
+});
